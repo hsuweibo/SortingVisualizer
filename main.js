e40c7137cab8d417db9eb8f019c143a469106b82
Weibo Hsu <hsuweibo@yahoo.com>
2020-06-21 03:47:28 -0400

    Implement animation and Bubble Sort.

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const btnBubble = document.getElementById("bubble");
+const btnReset = document.getElementById("reset");
+
+btnBubble.addEventListener('click', function(){
+    ab.animationSeq = bubbleSort(ab.toArray());
+    ab.playAnimation();
+})
+
+btnReset.addEventListener('click', function(){
+    ab.clear(ctx);
+    ab.initRandomArray();
+    ab.draw(ctx);
+    ab.animationSeq = [];
+})
+
 const canvas = document.getElementById("cv");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -20,6 +35,23 @@ Bar.prototype.draw = function(ctx){
     ctx.restore();
 }
 
+Bar.prototype.clear = function (ctx){
+    ctx.save();
+    ctx.clearRect(this.x, this.y, this.width, this.height);
+    ctx.restore();
+}
+
+Bar.prototype.setProperties = function(bp){
+    this.color = bp.color;
+    this.height = bp.height;
+}
+
+function BarProperties(height, color){
+    this.height = height;
+    this.color = color;
+}
+
+
 function ArrayBar(size, range, width, gap){
     this.range = range;
     this.size = size;
@@ -27,7 +59,9 @@ function ArrayBar(size, range, width, gap){
     this.gap = gap;
     this.bars = [];
     this.initRandomArray();
+    this.animationSeq = [];
 }
+
 
 ArrayBar.prototype.initRandomArray = function(){
     const startx = Math.floor((canvas.width - (this.width + this.gap) * this.size) / 2);
@@ -41,15 +75,112 @@ ArrayBar.prototype.initRandomArray = function(){
     }
 }
 
+
+ArrayBar.prototype.addAnimationSeq = function (changes){
+    this.animationSeq.push(changes);
+}
+
 ArrayBar.prototype.draw = function (ctx){
     for (const bar of this.bars) {
         bar.draw(ctx);
     }
 }
 
+ArrayBar.prototype.clear = function (ctx){
+    for (const bar of this.bars) {
+        bar.clear(ctx);
+    }
+}
 
-const test = new ArrayBar(20, 500, 30, 5);
-test.draw(ctx);
+ArrayBar.prototype.playAnimation = function(){
+    function update(){
+        console.log(this.animationSeq.length);
+        if (this.animationSeq.length > 0){
+            let changes = this.animationSeq.shift();
+            for (const prop in changes){
+                if (changes.hasOwnProperty(prop)){
+                    let index = prop.toString();
+                    let bar = this.bars[index];
+                    bar.clear(ctx);
+                    bar.setProperties(changes[index]);
+                    bar.draw(ctx);
+                }
+            }
+            // setTimeout(boundUpdate, 0.01);
+        }else{
+            clearInterval(animationId);
+        }
+    }
+    const boundUpdate = update.bind(this);
+    var animationId = setInterval(boundUpdate, 100);
+}
+
+ArrayBar.prototype.toArray = function(){
+    const array = [];
+    for (let i = 0; i < this.bars.length; i++){
+        array.push(this.bars[i].height);
+    }
+    return array;
+}
+
+function bubbleSort(array){
+    const animationSeq = []
+    for (let i = 0; i < array.length - 1; i ++){
+        let j = array.length - 2; 
+        while (j >= i){
+
+            let compare_color = {
+                [j]: new BarProperties(array[j], 'green'), 
+                [j+1]: new BarProperties(array[j + 1], 'green')
+            };
+            animationSeq.push(compare_color);
+
+            if (array[j] > array[j + 1]){
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                
+                
+                
+                let swap_color = {
+                    [j]: new BarProperties(array[j], 'red'), 
+                    [j+1]: new BarProperties(array[j+1], 'red')
+                }
 
 
+
+                animationSeq.push(swap_color);
+            }
+
+            let compare_uncolor = {
+                [j]: new BarProperties(array[j], 'grey'), 
+                [j+1]: new BarProperties(array[j + 1], 'grey')
+            };
+            animationSeq.push(compare_uncolor);
+            j--;
+        }
+
+        let color_sorted = {
+            [i]: new BarProperties(array[j + 1], 'brown')
+        };
+        animationSeq.push(color_sorted);
+    }
+
+    let last_sorted = {
+        [array.length - 1]: new BarProperties(array[array.length - 1], 'brown')
+    };
+    animationSeq.push(last_sorted);
+
+    return animationSeq;
+}
+
+
+const ab = new ArrayBar(50, 800, 5, 2);
+ab.draw(ctx);
+
+
+
+// ab.animationSeq = bubbleSort(ab.toArray());
+
+// ab.playAnimation();
 
